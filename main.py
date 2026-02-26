e23d1b4dab698ee79519cb84eae79dbfdfad2cf6
Erick Gomez <eerick.16@gmail.com>
2026-02-26 16:56:17 -0600

    blink led in main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from micropython-fw!")
+import time
+
+from machine import Pin  # type: ignore
+
+
+def blink_led():
+    pin_led = Pin(2, Pin.OUT)
+    while True:
+        pin_led.on()
+        time.sleep(1)
+        pin_led.off()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    blink_led()
