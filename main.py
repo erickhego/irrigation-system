71a4118cef317a95e777c933f0b39a663ae91c24
Erick Gomez <eerick.16@gmail.com>
2026-03-06 07:19:17 -0600

    Humidity sensor working

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import time
 
-from machine import Pin  # type: ignore
+import ntptime  # type: ignore
+from machine import ADC, Pin  # type: ignore
+
+MAX = 65535
+MIN = 34000
+D0 = Pin(0, Pin.OUT)
+
+ntptime.settime()
 
 
-def blink_led():
-    pin_led = Pin(2, Pin.OUT)
-    while True:
-        pin_led.on()
-        time.sleep(1)
-        pin_led.off()
-        time.sleep(1)
+def humidity_sensor():
+    adc = ADC(0)
+    D0.on()
+
+    lectures_range = MAX - MIN
+    lecture = adc.read_u16()
+    percentage = ((MAX - lecture) / lectures_range) * 100
+    D0.off()
+
+    return (lecture, 100 if percentage > 100 else percentage)
 
 
 if __name__ == "__main__":
-    blink_led()
+    D0.off()
+    time.sleep(10)
+    print("Iniciando lectura")
+    while True:
+        humidity, h_percentage = humidity_sensor()
+        date_time = time.localtime(time.time() + (-6 * 3600))
+        date_time_stamp = f"{date_time[0]}-{date_time[1]:02d}-{date_time[2]:02d} {date_time[3]:02d}:{date_time[4]:02d}:{date_time[5]:02d}"
+        print(
+            f"datetime: {date_time_stamp} Lectura: {humidity} - porcetaje: {h_percentage}"
+        )
+        # Aqui irá el POST a la API
+        time.sleep(5)
